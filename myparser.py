c0b836269212a673e1ad840a8cec1b5f79d33df5
Alberto Massidda <albertomassidda@fb.com>
2021-09-03 23:16:59 +0200

    Initial commit

---
+++ myparser.py
@@ -0,0 +1,111 @@
+
+EOF, INTEGER, PLUS, MINUS = 'EOF', 'INTEGER', 'PLUS', 'MINUS'
+# token: models content and type of data
+class Token(object):
+
+    def __init__(self, type, value):
+        self.type = type
+        self.value = value
+
+    def __str__(self):
+        return "TOKEN({type}, {value})".format(type=self.type, value=self.value)
+
+    def __repr__(self):
+        return self.__str__()
+
+
+
+# lexer: parses string and returns tokens
+
+class Lexer(object):
+
+    def __init__(self, text):
+        self._text = text
+        self._current_char = None
+        self._current_position = -1
+
+    def advance(self):
+        """read another unit of the input in the current state"""
+        # check pointer bounds
+        if (self._current_position + 1) == len(self._text):
+            return Token(EOF, None)
+
+        # advance pointer
+        self._current_position+=1
+
+        self._current_char = self._text[self._current_position]
+
+    def get_next_token(self):
+        """interpret current state into a token"""
+
+        # get current char
+        self.advance()
+
+        # infer a symbol
+        symbol = None
+
+        if self._current_char == '+':
+            symbol = PLUS
+            token = Token(symbol, self._current_char)
+        if self._current_char == '-':
+            symbol = MINUS
+            token = Token(symbol, self._current_char)
+        if self._current_char.isdigit():
+            symbol = INTEGER
+            token = Token(symbol, int(self._current_char))
+        if symbol == None:
+            raise Exception("Can't parse symbol {symbol}".format(symbol=self._current_char))
+
+        # return a token
+        return token
+
+
+# interpreter: sends string and judges order of token into actual actions
+class Interpreter(object):
+    def __init__(self, text):
+        self._lexer = Lexer(text)
+        self._current_token = self._lexer.get_next_token()
+
+    def eat(self, symbol):
+        """consume the lexer tape with an expected symbol. tape doesn't advance if symbol is unexpected"""
+        if self._current_token.type == symbol:
+            token = self._lexer.get_next_token()
+            self._current_token = token
+        else:
+            raise Exception("Unexpected symbol {symbol}".format(symbol=symbol))
+
+    def term(self):
+        """return current token as a terminal (INTEGER) and consume the tape"""
+        token = self._current_token.value
+        self.eat(INTEGER)
+        return token
+
+    def expr(self):
+        """the FSM that expects a sequence of terms"""
+        left = self.term()
+
+        if self._current_token.type == PLUS:
+            self.eat(PLUS)
+            right = self.term()
+            result = left + right
+        if self._current_token.type == MINUS:
+            self.eat(MINUS)
+            right = self._current_token.value
+            right = self.term()
+            result = left - right
+        print(result)
+
+
+
+# main: interactive prompt
+
+if __name__ == "__main__":
+    while True:
+        try:
+            text = str(raw_input("calc> "))
+            i = Interpreter(text)
+            i.expr()
+        except Exception, e:
+            pass
+        if len(text) == 0:
+            break
